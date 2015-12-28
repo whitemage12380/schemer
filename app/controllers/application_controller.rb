c8f4ee3ea307f29c6ea6b53a99d19b73b949ebd5
Egan Neuhengen <egan@neuhengen.net>
2015-12-27 21:55:32 -0800

    Set up new features for Devise, beginnings of basic layout view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < ActionController::Base
+  # Specify the correct layout using the logic in the below specify_layout method
+  layout :specify_layout
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Before actions
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
+  # Handle strong parameters for custom Devise user attributes
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :display_name, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :display_name, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :display_name, :email, :password, :password_confirmation, :current_password) }
+  end
+
+  # Show main layout unless signed out, in which case use sign in layout
+  def specify_layout
+    if devise_controller?
+      #if is_a?(RegistrationsController)
+      if !(current_user.nil?)
+        "application"
+      else
+        "sign_in"
+      end
+    else
+      "application"
+    end
+  end
 end
