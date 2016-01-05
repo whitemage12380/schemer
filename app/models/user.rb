d197ab722bbce1e184d6c9982ebcace9b6869ed3
Egan Neuhengen <egan.neuhengen@nwea.org>
2016-01-04 21:37:04 -0800

    Sign in now through email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Do not allow @ in usernames to avoid conflicts with email addresses
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  attr_accessor :login
+
+  # Check username and email for login, use whichever fits
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions.to_h).first
+    end
+  end
 end
