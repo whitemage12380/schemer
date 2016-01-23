0bb01a00f22186669545d042eb753447e3fc226c
Egan Neuhengen <lightningworks@gmail.com>
2016-01-23 14:55:34 -0800

    First attempt at task list item styling

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   # Associations
+  belongs_to :user
+  belongs_to :task_category, foreign_key: "category_id"
   has_many :tasks, class_name: "Task", foreign_key: "parent_task_id", dependent: :destroy
 end
