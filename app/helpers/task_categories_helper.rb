2238bfcba92db7de0bc4c45cee96d0bd24e5b7ba
Egan Neuhengen <lightningworks@gmail.com>
2016-03-05 23:24:43 -0800

    Task category menu item highlights when on that screen

--- app/helpers/task_categories_helper.rb
+++ app/helpers/task_categories_helper.rb
@@ -1,2 +1,9 @@
 module TaskCategoriesHelper
+
+  def is_active_category(page_name)
+    if params[:controller] == "task_categories" && params[:action] == "show" && params[:id] == page_name
+      "active"
+    end
+  end
+
 end
